c421ee0b93c7de83ec672b543550adf9b0c03227
YWak <ywak1984@gmail.com>
2019-09-29 01:02:12 +0900

    提出した

--- ABC/142/E/main.go
+++ ABC/142/E/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"container/heap"
 	"fmt"
 	"os"
 	"strconv"
@@ -12,106 +11,48 @@ func main() {
 	N := nextInt()
 	M := nextInt()
 
-	p := PrimalDual{N: 1 + M + N + 1, graph: make([][]edge, 1+M+N+1)} // S + 鍵 + 宝箱 + T
-	for i := 1; i <= M; i++ {
-		a := nextInt()
+	A := make([]int, M)
+	C := make([]int, M)
+
+	for i := 0; i < M; i++ {
+		A[i] = nextInt()
 		b := nextInt()
 
-		addEdge(p, 0, i, b, a)
-
 		for j := 0; j < b; j++ {
-			c := nextInt()
-			addEdge(p, i, M+c, 1, 0)
+			c := nextInt() - 1
+			C[i] |= (1 << uint(c))
 		}
 	}
-	for i := 1; i <= N; i++ {
-		addEdge(p, M+i, 1+M+N, 1, 0)
+	inf := 1 << 29
+
+	max := 1 << uint(N)
+	dp := make([]int, max)
+
+	for i := 0; i < max; i++ {
+		dp[i] = inf
 	}
+	dp[0] = 0
 
-	fmt.Println(minCostFlow(p, 0, len(p.graph)-1, inf))
-}
-
-var inf = 1 << 29
-
-// PrimalDual is min cost flow graph
-type PrimalDual struct {
-	N     int
-	graph [][]edge
-}
-
-type edge struct {
-	to   int
-	cap  int
-	cost int
-	rev  int
-}
-
-func addEdge(p PrimalDual, from, to, cap, cost int) {
-	p.graph[from] = append(p.graph[from], edge{to, cap, cost, len(p.graph[to])})
-	p.graph[to] = append(p.graph[to], edge{from, 0, -cost, len(p.graph[from]) - 1})
-}
-
-func minCostFlow(p PrimalDual, start, goal, flow int) int {
-	prevNode := make([]int, p.N)
-	prevEdge := make([]int, p.N)
-	potential := make([]int, p.N)
-	total := 0
-
-	for flow > 0 {
-		dist := make([]int, p.N)
-		for i := 0; i < p.N; i++ {
-			dist[i] = inf
+	for s := 0; s < max; s++ {
+		for i := 0; i < M; i++ {
+			t := s | C[i]
+			cost := dp[s] + A[i]
+			dp[t] = min(dp[t], cost)
 		}
-
-		pq := make(pqueue, p.N)
-		heap.Push(&pq, &node{value: start, priority: 0})
 	}
 
-	return total
-}
-
-type node struct {
-	value    int
-	priority int
-	index    int
-}
-
-type pqueue []*node
-
-// キューの長さ
-func (pq pqueue) Len() int { return len(pq) }
-
-// キューの大小比較
-// 基本は大きい方を優先する
-func (pq pqueue) Less(i, j int) bool { return pq[i].priority > pq[j].priority }
-
-// キューの内容交換
-func (pq pqueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
-}
-
-// キューへの追加
-func (pq *pqueue) Push(x interface{}) {
-	n := len(*pq)
-	i := x.(*node)
-	i.index = n
-	*pq = append(*pq, x.(*node))
-}
-
-// キューからの取り出し
-func (pq *pqueue) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	if n == 0 {
-		fmt.Println("queue is empty")
-		return nil
+	ans := dp[max-1]
+	if ans == inf {
+		ans = -1
 	}
-	i := old[n-1]
-	i.index = -1
-	*pq = old[0 : n-1]
-	return i
+	fmt.Println(ans)
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 var stdin = initStdin()
