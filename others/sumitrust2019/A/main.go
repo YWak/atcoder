00464a7dbcb60127b3038b9c36503741eb81ff1d
YWak <ywak1984@gmail.com>
2019-12-01 21:02:42 +0900

    提出した

---
+++ others/sumitrust2019/A/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	m1 := nextInt()
+	nextInt()
+	m2 := nextInt()
+	nextInt()
+
+	if m1 != m2 {
+		fmt.Println("1")
+	} else {
+		fmt.Println("0")
+	}
+}
+
+var stdin = initStdin()
+
+func initStdin() *bufio.Scanner {
+	bufsize := 1 * 1024 * 1024 // 1 MB
+	var stdin = bufio.NewScanner(os.Stdin)
+	stdin.Buffer(make([]byte, bufsize), bufsize)
+	stdin.Split(bufio.ScanWords)
+	return stdin
+}
+
+func nextString() string {
+	stdin.Scan()
+	return stdin.Text()
+}
+
+func nextBytes() []byte {
+	stdin.Scan()
+	return stdin.Bytes()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(nextString())
+	return i
+}
+
+func nextInt64() int64 {
+	i, _ := strconv.ParseInt(nextString(), 10, 64)
+	return i
+}
