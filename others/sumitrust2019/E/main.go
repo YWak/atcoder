0312ebdd5536024efda20614634981d8bd0b51ac
YWak <ywak1984@gmail.com>
2019-12-01 23:15:41 +0900

    提出した

---
+++ others/sumitrust2019/E/main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	N := nextInt()
+	A := make([]int64, N+1)
+
+	for i := 1; i <= N; i++ {
+		A[i] = nextInt64()
+	}
+
+	t := mint(1)
+	dp := [3]int64{0, 0, 0}
+
+	for i := 1; i <= N; i++ {
+		c := mint(0)
+		for j := 0; j < 3; j++ {
+			if dp[j] == A[i] {
+				if c == 0 {
+					dp[j]++
+				}
+				c++
+			}
+		}
+		t = t.mul(c)
+
+		// ソート
+		for k := 0; k < 3; k++ {
+			for l := k + 1; l < 3; l++ {
+				if dp[l] < dp[k] {
+					dp[k], dp[l] = dp[l], dp[k]
+				}
+			}
+		}
+	}
+
+	fmt.Println(t)
+}
+
+type mint int64
+
+var mod = mint(1e9 + 7)
+
+// add は a + bを返します
+func (a mint) add(b mint) mint {
+	return (a + b) % mod
+}
+
+// sub は a - bを返します
+func (a mint) sub(b mint) mint {
+	return (a - b + mod) % mod
+}
+
+// mul は a * bを返します
+func (a mint) mul(b mint) mint {
+	return (a * (b % mod)) % mod
+}
+
+// div は a/bを返します
+func (a mint) div(b mint) mint {
+	return a.mul(b.inv())
+}
+
+// inv は aの逆元を返します
+func (a mint) inv() mint {
+	// 拡張ユークリッドの互除法
+	b := mod
+	u := mint(1)
+	v := mint(0)
+	for b > 0 {
+		t := a / b
+		a -= t * b
+		a, b = b, a
+		u -= t * v
+		u, v = v, u
+	}
+	return (u + mod) % mod
+}
+
+// pow は a ^ bを返します
+func (a mint) pow(b mint) mint {
+	ans := mint(1)
+
+	for b != 0 {
+		if b&1 == 1 {
+			ans = ans.mul(a)
+		}
+		a = a.mul(a)
+		b = b >> 1
+	}
+	return ans
+}
+
+var stdin = initStdin()
+
+func initStdin() *bufio.Scanner {
+	bufsize := 1 * 1024 * 1024 // 1 MB
+	var stdin = bufio.NewScanner(os.Stdin)
+	stdin.Buffer(make([]byte, bufsize), bufsize)
+	stdin.Split(bufio.ScanWords)
+	return stdin
+}
+
+func nextString() string {
+	stdin.Scan()
+	return stdin.Text()
+}
+
+func nextBytes() []byte {
+	stdin.Scan()
+	return stdin.Bytes()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(nextString())
+	return i
+}
+
+func nextInt64() int64 {
+	i, _ := strconv.ParseInt(nextString(), 10, 64)
+	return i
+}
