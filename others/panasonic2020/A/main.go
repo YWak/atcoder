2dbe6b0e83698d527a9d80cf291544e495f079be
YWak <ywak1984@gmail.com>
2020-03-14 21:01:44 +0900

    提出した

---
+++ others/panasonic2020/A/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+
+	K := nextInt()
+	arr := []int{1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51}
+
+	fmt.Println(arr[K-1])
+}
+
+var stdin = initStdin()
+
+func initStdin() *bufio.Scanner {
+	bufsize := 1 * 1024 * 1024 // 1 MB
+	var stdin = bufio.NewScanner(os.Stdin)
+	stdin.Buffer(make([]byte, bufsize), bufsize)
+	stdin.Split(bufio.ScanWords)
+	return stdin
+}
+
+func nextString() string {
+	stdin.Scan()
+	return stdin.Text()
+}
+
+func nextBytes() []byte {
+	stdin.Scan()
+	return stdin.Bytes()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(nextString())
+	return i
+}
+
+func nextInt64() int64 {
+	i, _ := strconv.ParseInt(nextString(), 10, 64)
+	return i
+}
