2e1e88c4c1cb1e065b7e0f4a348c05f2f511938b
YWak <ywak1984@gmail.com>
2020-09-13 21:26:14 +0900

    提出した

---
+++ ABC/178/D/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	S := nextInt()
+
+	// dp[i] は 合計iになる数列の数
+	dp := make([]mint, S+10)
+	for i := 3; i <= S; i++ {
+		dp[i] = dp[i].add(mint(1))
+		for j := 3; j <= S-i; j++ {
+			// debug(i, j, dp[i], dp[i+j])
+			dp[i+j] = dp[i+j].add(dp[i])
+		}
+	}
+
+	fmt.Println(dp[S])
+}
+
+type mint int64
+
+var mod = mint(1e9 + 7)
+
+// add は a + bを返します
+func (a mint) add(b mint) mint {
+	return (a + b) % mod
+}
+
+// sub は a - bを返します
+func (a mint) sub(b mint) mint {
+	return (a - b + mod) % mod
+}
+
+// mul は a * bを返します
+func (a mint) mul(b mint) mint {
+	return (a * (b % mod)) % mod
+}
+
+// div は a/bを返します
+func (a mint) div(b mint) mint {
+	return a.mul(b.inv())
+}
+
+// inv は aの逆元を返します
+func (a mint) inv() mint {
+	// 拡張ユークリッドの互除法
+	b := mod
+	u := mint(1)
+	v := mint(0)
+	for b > 0 {
+		t := a / b
+		a -= t * b
+		a, b = b, a
+		u -= t * v
+		u, v = v, u
+	}
+	return (u + mod) % mod
+}
+
+// pow は a ^ bを返します
+func (a mint) pow(b mint) mint {
+	ans := mint(1)
+
+	for b != 0 {
+		if b&1 == 1 {
+			ans = ans.mul(a)
+		}
+		a = a.mul(a)
+		b = b >> 1
+	}
+	return ans
+}
+
+var stdin = initStdin()
+
+func initStdin() *bufio.Scanner {
+	bufsize := 1 * 1024 * 1024 // 1 MB
+	var stdin = bufio.NewScanner(os.Stdin)
+	stdin.Buffer(make([]byte, bufsize), bufsize)
+	stdin.Split(bufio.ScanWords)
+	return stdin
+}
+
+func nextString() string {
+	stdin.Scan()
+	return stdin.Text()
+}
+
+func nextBytes() []byte {
+	stdin.Scan()
+	return stdin.Bytes()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(nextString())
+	return i
+}
+
+func nextInt64() int64 {
+	i, _ := strconv.ParseInt(nextString(), 10, 64)
+	return i
+}
+
+func debug(args ...interface{}) {
+	fmt.Fprintln(os.Stderr, args...)
+}
