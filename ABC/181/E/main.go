a112ea89415de572651eeb726ecbf6184e19e978
YWak <ywak1984@gmail.com>
2020-11-01 22:24:20 +0900

    TLE回避

--- ABC/181/E/main.go
+++ ABC/181/E/main.go
@@ -27,6 +27,29 @@ func main() {
 	sort.Slice(W, func(i, j int) bool { return W[i] < W[j] })
 
 	ans := math.MaxInt64
+
+	if N == 1 {
+		for i := 0; i < M; i++ {
+			ans = min(ans, abs(H[0]-W[i]))
+		}
+		fmt.Println(ans)
+		return
+	}
+
+	// D1[n] は 前からn番目までのペアの累積和
+	D1 := make([]int, N/2+1)
+	// D2[n] は 後ろからn番目までのペアの累積和
+	D2 := make([]int, N/2+1)
+
+	D1[0] = 0
+	D2[0] = 0
+
+	for i := 1; i <= N/2; i++ {
+		j := (i - 1) * 2
+		D1[i] = D1[i-1] + abs(H[j]-H[j+1])
+		D2[i] = D2[i-1] + abs(H[N-1-j]-H[N-2-j])
+	}
+
 	for w := 0; w < M; w++ {
 		// 先生の位置 == W[w] が入って昇順になる位置。
 		j := -1
@@ -43,29 +66,16 @@ func main() {
 
 		// 差分を取る
 		c := 0
-		debug("j =", j)
+		// debug("j =", j)
 		if (j+2)%2 == 1 {
-			for i := 0; i <= j; i += 2 {
-				// debug("i, i+1", i, i+1)
-				c += abs(H[i] - H[i+1])
-			}
-			debug("i, j", j+1)
-			c += abs(H[j+1] - W[w])
-			for i := j + 2; i < N; i += 2 {
-				// debug("i, i+1", i, i+1)
-				c += abs(H[i] - H[i+1])
-			}
+			// 前半
+			c += D1[(j+1)/2]
+			c += abs(W[w] - H[j+1])
+			c += D2[(N-j-1)/2]
 		} else {
-			for i := 0; i < j; i += 2 {
-				// debug("i, i+1", i, i+1)
-				c += abs(H[i] - H[i+1])
-			}
-			debug("i, j")
-			c += abs(H[j] - W[w])
-			for i := j + 1; i < N; i += 2 {
-				// debug("i, i+1", i, i+1)
-				c += abs(H[i] - H[i+1])
-			}
+			c += D1[j/2]
+			c += abs(W[w] - H[j])
+			c += D2[(N-j)/2]
 		}
 		ans = min(ans, c)
 	}
@@ -117,5 +127,5 @@ func nextInt64() int64 {
 }
 
 func debug(args ...interface{}) {
-	// fmt.Fprintln(os.Stderr, args...)
+	fmt.Fprintln(os.Stderr, args...)
 }
