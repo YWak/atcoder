d60e3f5abbe8918dd9b730ffddd41dbb24d280c5
YWak <ywak1984@gmail.com>
2020-11-08 21:35:51 +0900

    提出した

---
+++ ABC/182/D/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func main() {
+	N := nextInt()
+	A := nextInts(N)
+
+	x := 0
+	ans := math.MinInt64
+	v := 0
+	vmax := 0
+
+	for i := 0; i < N; i++ {
+		prev := x
+		v += A[i]
+		x += v
+		vmax = max(vmax, v)
+		ans = max(ans, prev+x)
+		ans = max(ans, prev+vmax)
+	}
+
+	fmt.Println(ans)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+var stdin = initStdin()
+
+func initStdin() *bufio.Scanner {
+	bufsize := 1 * 1024 * 1024 // 1 MB
+	var stdin = bufio.NewScanner(os.Stdin)
+	stdin.Buffer(make([]byte, bufsize), bufsize)
+	stdin.Split(bufio.ScanWords)
+	return stdin
+}
+
+func nextString() string {
+	stdin.Scan()
+	return stdin.Text()
+}
+
+func nextBytes() []byte {
+	stdin.Scan()
+	return stdin.Bytes()
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(nextString())
+	return i
+}
+
+func nextInts(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt()
+	}
+	return a
+}
+
+func debug(args ...interface{}) {
+	fmt.Fprintln(os.Stderr, args...)
+}
