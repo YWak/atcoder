1bf4e1962a3eef65ae0c6d093b78d6c36145ca6b
YWak <ywak1984@gmail.com>
2020-11-16 00:25:06 +0900

    提出した

--- ABC/183/E/main.go
+++ ABC/183/E/main.go
@@ -7,111 +7,68 @@ import (
 	"strconv"
 )
 
-func main() {
-	H := nextInt()
-	W := nextInt()
+var H int
+var W int
+var S [][]byte
 
-	S := make([][]byte, H)
+func solve() int {
+	dp := make([][]int, H)
+	x := make([][]int, H)
+	y := make([][]int, H)
+	z := make([][]int, H)
 	for i := 0; i < H; i++ {
-		S[i] = nextBytes()
+		dp[i] = make([]int, W)
+		x[i] = make([]int, W)
+		y[i] = make([]int, W)
+		z[i] = make([]int, W)
 	}
 
-	dp := [2000][2000]mint{}
 	dp[0][0] = 1
-
-	for h := 0; h < H; h++ {
-		for w := 0; w < W; w++ {
-			if S[h][w] == '#' {
+	for i := 0; i < H; i++ {
+		for j := 0; j < W; j++ {
+			if i == 0 && j == 0 {
 				continue
 			}
+			if S[i][j] == '#' {
+				continue
+			}
+			if j > 0 {
+				x[i][j] = (x[i][j-1] + dp[i][j-1]) % mod
+			}
+			if i > 0 {
+				y[i][j] = (y[i-1][j] + dp[i-1][j]) % mod
+			}
+			if i > 0 && j > 0 {
+				z[i][j] = (z[i-1][j-1] + dp[i-1][j-1]) % mod
+			}
+			dp[i][j] = (x[i][j] + y[i][j] + z[i][j]) % mod
+		}
+	}
+	return dp[H-1][W-1]
+}
 
-			// 縦
-			for i := h + 1; i < H; i++ {
-				if S[i][w] == '#' {
-					break
-				}
-				dp[i][w] = dp[i][w].add(dp[h][w])
-			}
-			// 横
-			for i := w + 1; i < W; i++ {
-				if S[h][i] == '#' {
-					break
-				}
-				dp[h][i] = dp[h][i].add(dp[h][w])
-			}
-			// 斜め
-			for i := 1; h+i < H && w+i < W; i++ {
-				if S[h+i][w+i] == '#' {
-					break
-				}
-				dp[h+i][w+i] = dp[h+i][w+i].add(dp[h][w])
-			}
+func main() {
+	wr := bufio.NewWriter(os.Stdout)
+	defer wr.Flush()
+
+	H = nextInt()
+	W = nextInt()
+
+	S = make([][]byte, H)
+	for i := 0; i < H; i++ {
+		S[i] = nextBytes()
+
+		if len(S[i]) != W {
+			debug("NG!!", len(S[i]), W)
 		}
 	}
 
-	fmt.Println(dp[H-1][W-1])
-}
-
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
+	fmt.Fprintln(wr, solve())
 }
 
 type mint int
 
-const mod = mint(1e9 + 7)
-
-// add は a + bを返します
-func (a mint) add(b mint) mint {
-	return (a + b) % mod
-}
-
-// sub は a - bを返します
-func (a mint) sub(b mint) mint {
-	return (a - b + mod) % mod
-}
-
-// mul は a * bを返します
-func (a mint) mul(b mint) mint {
-	return (a * (b % mod)) % mod
-}
-
-// div は a/bを返します
-func (a mint) div(b mint) mint {
-	return a.mul(b.inv())
-}
-
-// inv は aの逆元を返します
-func (a mint) inv() mint {
-	// 拡張ユークリッドの互除法
-	b := mod
-	u := mint(1)
-	v := mint(0)
-	for b > 0 {
-		t := a / b
-		a -= t * b
-		a, b = b, a
-		u -= t * v
-		u, v = v, u
-	}
-	return (u + mod) % mod
-}
-
-// pow は a ^ bを返します
-func (a mint) pow(b mint) mint {
-	ans := mint(1)
-
-	for b > 0 {
-		if b&1 == 1 {
-			ans = ans.mul(a)
-		}
-		a = a.mul(a)
-		b = b >> 1
-	}
-	return ans
-}
+const mod = int(1e9 + 7)
 
 var stdin = initStdin()
 
@@ -123,18 +80,18 @@ func initStdin() *bufio.Scanner {
 	return stdin
 }
 
-func nextString() string {
+// NextString は標準入力から文字列を読み込みます。
+func NextString() string {
 	stdin.Scan()
 	return stdin.Text()
 }
 
 func nextBytes() []byte {
-	stdin.Scan()
-	return stdin.Bytes()
+	return []byte(NextString())
 }
 
 func nextInt() int {
-	i, _ := strconv.Atoi(nextString())
+	i, _ := strconv.Atoi(NextString())
 	return i
 }
 
