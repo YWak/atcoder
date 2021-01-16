23839ef104a4fed1fcea697a454c049a516a6656
YWak <ywak1984@gmail.com>
2021-01-17 03:04:05 +0900

    提出した

---
+++ ARC/002/C/main.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"math/bits"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// INF は最大値を表す数
+const INF = int(1e9)
+
+func main() {
+	N := nextInt()
+	C := nextString()
+
+	s := "ABXY"
+	ans := INF
+	for a := 0; a < 4; a++ {
+		for b := 0; b < 4; b++ {
+			for c := 0; c < 4; c++ {
+				for d := 0; d < 4; d++ {
+					if a == c && b == d {
+						continue
+					}
+					dp := make([]int, N+1)
+					dp[1] = 1
+					for i := 1; i < N; i++ {
+						dp[i+1] = min(dp[i]+1, dp[i-1]+2)
+						if C[i-1] == s[a] && C[i] == s[b] {
+							dp[i+1] = min(dp[i+1], dp[i-1]+1)
+						} else if C[i-1] == s[c] && C[i] == s[d] {
+							dp[i+1] = min(dp[i+1], dp[i-1]+1)
+						}
+					}
+
+					ans = min(ans, dp[N])
+				}
+			}
+		}
+	}
+	fmt.Println(ans)
+}
+
+func debug(args ...interface{}) {
+	fmt.Fprintln(os.Stderr, args...)
+}
+
+// ==================================================
+// 入力操作
+// ==================================================
+var stdin = initStdin()
+
+func initStdin() *bufio.Scanner {
+	bufsize := 1 * 1024 * 1024 // 1 MB
+	var stdin = bufio.NewScanner(os.Stdin)
+	stdin.Buffer(make([]byte, bufsize), bufsize)
+	stdin.Split(bufio.ScanWords)
+	return stdin
+}
+
+func nextString() string {
+	stdin.Scan()
+	return stdin.Text()
+}
+
+// 遅いから極力使わない。
+func nextBytes() []byte {
+	return []byte(nextString())
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(nextString())
+	return i
+}
+
+func nextInts(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt()
+	}
+	return a
+}
+
+// toi は byteの数値をintに変換します。
+func toi(b byte) int {
+	return int(b - '0')
+}
+
+func nextLongIntAsArray() []int {
+	s := nextString()
+	l := len(s)
+	arr := make([]int, l)
+	for i := 0; i < l; i++ {
+		arr[i] = toi(s[i])
+	}
+
+	return arr
+}
+
+func nextFloat() float64 {
+	f, _ := strconv.ParseFloat(nextString(), 64)
+	return f
+}
+
+// ==================================================
+// 数値操作
+// ==================================================
+
+// max は aとbのうち大きい方を返します。
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// min は aとbのうち小さい方を返します。
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// abs は aの絶対値を返します。
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+// pow は aのb乗を返します。
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+// binarysearch は judgeがtrueを返す最小の数値を返します。
+func binarysearch(ok, ng int, judge func(int) bool) int {
+	for abs(ok-ng) > 1 {
+		mid := (ok + ng) / 2
+
+		if judge(mid) {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+
+	return ok
+}
+
+// ==================================================
+// ビット操作
+// ==================================================
+
+// nthbit はaのn番目のビットを返します。
+func nthbit(a int, n int) int { return int((a >> uint(n)) & 1) }
+
+// popcount はaのうち立っているビットを数えて返します。
+func popcount(a int) int {
+	return bits.OnesCount(uint(a))
+}
+
+// ==================================================
+// 文字列操作
+// ==================================================
+
+// toLowerCase は sをすべて小文字にした文字列を返します。
+func toLowerCase(s string) string {
+	return strings.ToLower(s)
+}
+
+// toUpperCase は sをすべて大文字にした文字列を返します。
+func toUpperCase(s string) string {
+	return strings.ToUpper(s)
+}
+
+// isLower はbが小文字かどうかを判定します
+func isLower(b byte) bool {
+	return 'a' <= b && b <= 'z'
+}
+
+// isUpper はbが大文字かどうかを判定します
+func isUpper(b byte) bool {
+	return 'A' <= b && b <= 'Z'
+}
+
+// ==================================================
+// 構造体
+// ==================================================
+
+// Point は 座標を表す構造体です。
+type Point struct {
+	x int
+	y int
+}
+
+// Pointf は座標を表す構造体です。
+type Pointf struct {
+	x float64
+	y float64
+}
