a39a7cb85342b47a5e28907285ad7c2e8a854767
YWak <ywak1984@gmail.com>
2021-01-28 23:01:03 +0900

    提出した

---
+++ others/nikkei201-2-qual/B/main.go
@@ -0,0 +1,268 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"math/bits"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// INF は最大値を表す数
+const INF = int(1e9)
+
+func main() {
+	N := nextInt()
+	bucket := make([]int, N)
+	D := make([]int, N)
+	M := 0
+	for i := 0; i < N; i++ {
+		D[i] = nextInt()
+		bucket[D[i]]++
+		M = max(M, D[i])
+	}
+	if D[0] != 0 || bucket[0] != 1 {
+		fmt.Println(0)
+		return
+	}
+	// bucket check
+	for i := 0; i <= M; i++ {
+		if bucket[i] == 0 {
+			fmt.Println(0)
+			return
+		}
+	}
+
+	// つなぐ方法は b[i-1] ^ b[i] パターンある。
+	ans := mint(1)
+	for i := 1; i <= M; i++ {
+		ans = ans.mul(mint(bucket[i-1]).pow(mint(bucket[i])))
+	}
+
+	fmt.Println(ans)
+}
+
+func debug(args ...interface{}) {
+	fmt.Fprintln(os.Stderr, args...)
+}
+
+type mint int
+
+const mod = mint(998244353)
+
+// add は a + bを返します
+func (a mint) add(b mint) mint {
+	return (a + b) % mod
+}
+
+// sub は a - bを返します
+func (a mint) sub(b mint) mint {
+	return (a - b + mod) % mod
+}
+
+// mul は a * bを返します
+func (a mint) mul(b mint) mint {
+	return (a * (b % mod)) % mod
+}
+
+// div は a/bを返します
+func (a mint) div(b mint) mint {
+	return a.mul(b.inv())
+}
+
+// inv は aの逆元を返します
+func (a mint) inv() mint {
+	// 拡張ユークリッドの互除法
+	b := mod
+	u := mint(1)
+	v := mint(0)
+	for b > 0 {
+		t := a / b
+		a -= t * b
+		a, b = b, a
+		u -= t * v
+		u, v = v, u
+	}
+	return (u + mod) % mod
+}
+
+// pow は a ^ bを返します
+func (a mint) pow(b mint) mint {
+	ans := mint(1)
+
+	for b > 0 {
+		if b&1 == 1 {
+			ans = ans.mul(a)
+		}
+		a = a.mul(a)
+		b = b >> 1
+	}
+	return ans
+}
+
+// ==================================================
+// 入力操作
+// ==================================================
+var stdin = initStdin()
+
+func initStdin() *bufio.Scanner {
+	bufsize := 1 * 1024 * 1024 // 1 MB
+	var stdin = bufio.NewScanner(os.Stdin)
+	stdin.Buffer(make([]byte, bufsize), bufsize)
+	stdin.Split(bufio.ScanWords)
+	return stdin
+}
+
+func nextString() string {
+	stdin.Scan()
+	return stdin.Text()
+}
+
+// 遅いから極力使わない。
+func nextBytes() []byte {
+	return []byte(nextString())
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(nextString())
+	return i
+}
+
+func nextInts(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt()
+	}
+	return a
+}
+
+// toi は byteの数値をintに変換します。
+func toi(b byte) int {
+	return int(b - '0')
+}
+
+func nextLongIntAsArray() []int {
+	s := nextString()
+	l := len(s)
+	arr := make([]int, l)
+	for i := 0; i < l; i++ {
+		arr[i] = toi(s[i])
+	}
+
+	return arr
+}
+
+func nextFloat() float64 {
+	f, _ := strconv.ParseFloat(nextString(), 64)
+	return f
+}
+
+// ==================================================
+// 数値操作
+// ==================================================
+
+// max は aとbのうち大きい方を返します。
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// min は aとbのうち小さい方を返します。
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// abs は aの絶対値を返します。
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+// pow は aのb乗を返します。
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+// binarysearch は judgeがtrueを返す最小の数値を返します。
+func binarysearch(ok, ng int, judge func(int) bool) int {
+	for abs(ok-ng) > 1 {
+		mid := (ok + ng) / 2
+
+		if judge(mid) {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+
+	return ok
+}
+
+// ch は condがtrueのときok, falseのときngを返します。
+func ch(cond bool, ok, ng int) int {
+	if cond {
+		return ok
+	}
+	return ng
+}
+
+// ==================================================
+// ビット操作
+// ==================================================
+
+// nthbit はaのn番目のビットを返します。
+func nthbit(a int, n int) int { return int((a >> uint(n)) & 1) }
+
+// popcount はaのうち立っているビットを数えて返します。
+func popcount(a int) int {
+	return bits.OnesCount(uint(a))
+}
+
+// ==================================================
+// 文字列操作
+// ==================================================
+
+// toLowerCase は sをすべて小文字にした文字列を返します。
+func toLowerCase(s string) string {
+	return strings.ToLower(s)
+}
+
+// toUpperCase は sをすべて大文字にした文字列を返します。
+func toUpperCase(s string) string {
+	return strings.ToUpper(s)
+}
+
+// isLower はbが小文字かどうかを判定します
+func isLower(b byte) bool {
+	return 'a' <= b && b <= 'z'
+}
+
+// isUpper はbが大文字かどうかを判定します
+func isUpper(b byte) bool {
+	return 'A' <= b && b <= 'Z'
+}
+
+// ==================================================
+// 構造体
+// ==================================================
+
+// Point は 座標を表す構造体です。
+type Point struct {
+	x int
+	y int
+}
+
+// Pointf は座標を表す構造体です。
+type Pointf struct {
+	x float64
+	y float64
+}
