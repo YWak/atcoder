97c63e9a4636c7c3e2ead9cab5742bfd3bc33eda
YWak <ywak1984@gmail.com>
2021-02-01 17:28:06 +0900

    提出した

---
+++ ABC/180/E/main.go
@@ -0,0 +1,240 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"math/bits"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// INF は最大値を表す数
+const INF = int(1e9)
+
+type point struct {
+	x, y, z int
+}
+
+func main() {
+	N := nextInt()
+	p := make([]point, N)
+	dist := make([][]int, N)
+	for i := 0; i < N; i++ {
+		x := nextInt()
+		y := nextInt()
+		z := nextInt()
+		p[i] = point{x, y, z}
+		dist[i] = make([]int, N)
+	}
+	for i := 0; i < N; i++ {
+		for j := 0; j < N; j++ {
+			dist[i][j] = abs(p[i].x-p[j].x) + abs(p[i].y-p[j].y) + max(0, p[j].z-p[i].z)
+		}
+	}
+	// dp[S][i] は 都市集合Sに訪問済みで現在都市iにいるときの最小コスト
+	dp := make([][]int, 1<<N)
+	for i := 0; i < (1 << N); i++ {
+		dp[i] = make([]int, N)
+		for j := 0; j < N; j++ {
+			dp[i][j] = INF
+		}
+	}
+	dp[1][0] = 0
+	for c := 1; c < (1 << N); c++ {
+		if c&1 == 0 {
+			continue
+		}
+
+		// iにいてjに行こうとする遷移
+		for i := 0; i < N; i++ {
+			for j := 1; j < N; j++ {
+				if c&(1<<i) == 0 {
+					// iにいなければスキップ
+					continue
+				}
+				if c&(1<<j) != 0 {
+					// jに訪問済みならスキップ
+					continue
+				}
+				n := c | (1 << j)
+				dp[n][j] = min(dp[n][j], dp[c][i]+dist[i][j])
+			}
+		}
+	}
+	ans := INF
+	for i := 1; i < N; i++ {
+		ans = min(ans, dp[(1<<N)-1][i]+dist[i][0])
+	}
+	fmt.Println(ans)
+}
+
+func debug(args ...interface{}) {
+	fmt.Fprintln(os.Stderr, args...)
+}
+
+// ==================================================
+// 入力操作
+// ==================================================
+var stdin = initStdin()
+
+func initStdin() *bufio.Scanner {
+	bufsize := 1 * 1024 * 1024 // 1 MB
+	var stdin = bufio.NewScanner(os.Stdin)
+	stdin.Buffer(make([]byte, bufsize), bufsize)
+	stdin.Split(bufio.ScanWords)
+	return stdin
+}
+
+func nextString() string {
+	stdin.Scan()
+	return stdin.Text()
+}
+
+// 遅いから極力使わない。
+func nextBytes() []byte {
+	return []byte(nextString())
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(nextString())
+	return i
+}
+
+func nextInts(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt()
+	}
+	return a
+}
+
+// toi は byteの数値をintに変換します。
+func toi(b byte) int {
+	return int(b - '0')
+}
+
+func nextLongIntAsArray() []int {
+	s := nextString()
+	l := len(s)
+	arr := make([]int, l)
+	for i := 0; i < l; i++ {
+		arr[i] = toi(s[i])
+	}
+
+	return arr
+}
+
+func nextFloat() float64 {
+	f, _ := strconv.ParseFloat(nextString(), 64)
+	return f
+}
+
+// ==================================================
+// 数値操作
+// ==================================================
+
+// max は aとbのうち大きい方を返します。
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// min は aとbのうち小さい方を返します。
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// abs は aの絶対値を返します。
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+// pow は aのb乗を返します。
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+// binarysearch は judgeがtrueを返す最小の数値を返します。
+func binarysearch(ok, ng int, judge func(int) bool) int {
+	for abs(ok-ng) > 1 {
+		mid := (ok + ng) / 2
+
+		if judge(mid) {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+
+	return ok
+}
+
+// ch は condがtrueのときok, falseのときngを返します。
+func ch(cond bool, ok, ng int) int {
+	if cond {
+		return ok
+	}
+	return ng
+}
+
+// ==================================================
+// ビット操作
+// ==================================================
+
+// nthbit はaのn番目のビットを返します。
+func nthbit(a int, n int) int { return int((a >> uint(n)) & 1) }
+
+// popcount はaのうち立っているビットを数えて返します。
+func popcount(a int) int {
+	return bits.OnesCount(uint(a))
+}
+
+// ==================================================
+// 文字列操作
+// ==================================================
+
+// toLowerCase は sをすべて小文字にした文字列を返します。
+func toLowerCase(s string) string {
+	return strings.ToLower(s)
+}
+
+// toUpperCase は sをすべて大文字にした文字列を返します。
+func toUpperCase(s string) string {
+	return strings.ToUpper(s)
+}
+
+// isLower はbが小文字かどうかを判定します
+func isLower(b byte) bool {
+	return 'a' <= b && b <= 'z'
+}
+
+// isUpper はbが大文字かどうかを判定します
+func isUpper(b byte) bool {
+	return 'A' <= b && b <= 'Z'
+}
+
+// ==================================================
+// 構造体
+// ==================================================
+
+// Point は 座標を表す構造体です。
+type Point struct {
+	x int
+	y int
+}
+
+// Pointf は座標を表す構造体です。
+type Pointf struct {
+	x float64
+	y float64
+}
