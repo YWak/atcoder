02387d9f145ac600b1964e4badfa3e9292719af1
YWak <ywak1984@gmail.com>
2021-02-01 18:16:42 +0900

    提出した

---
+++ ABC/183/F/main.go
@@ -0,0 +1,242 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"math/bits"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// INF は最大値を表す数
+const INF = int(1e9)
+
+var roots []int
+
+func main() {
+	N := nextInt()
+	Q := nextInt()
+	maps := make([]map[int]int, N)
+	roots := make([]int, N)
+	for i := 0; i < N; i++ {
+		roots[i] = i
+		c := nextInt() - 1
+		maps[i] = map[int]int{c: 1}
+	}
+	add := func(m map[int]int, a, n int) {
+		v, ok := m[a]
+		if ok {
+			m[a] = v + n
+		} else {
+			m[a] = n
+		}
+	}
+	root := func(a int) int {
+		p := a
+		for p != roots[p] {
+			p = roots[p]
+		}
+		roots[a] = p
+		return p
+	}
+
+	for i := 0; i < Q; i++ {
+		t := nextInt()
+		if t == 1 {
+			a := nextInt() - 1
+			b := nextInt() - 1
+
+			// 合流は小さい方を大きい方へ
+			ra, rb := root(a), root(b)
+			if ra == rb {
+				continue
+			}
+			if len(maps[ra]) > len(maps[rb]) {
+				a, b = b, a
+				ra, rb = rb, ra
+			}
+			for k, v := range maps[ra] {
+				add(maps[rb], k, v)
+			}
+			roots[ra] = rb
+			// maps[ra] = nil
+			// debug(a, b, ra, rb)
+			// debug(maps)
+		} else {
+			x := nextInt() - 1
+			y := nextInt() - 1
+			fmt.Println(maps[root(x)][y])
+		}
+	}
+}
+
+func debug(args ...interface{}) {
+	fmt.Fprintln(os.Stderr, args...)
+}
+
+// ==================================================
+// 入力操作
+// ==================================================
+var stdin = initStdin()
+
+func initStdin() *bufio.Scanner {
+	bufsize := 1 * 1024 * 1024 // 1 MB
+	var stdin = bufio.NewScanner(os.Stdin)
+	stdin.Buffer(make([]byte, bufsize), bufsize)
+	stdin.Split(bufio.ScanWords)
+	return stdin
+}
+
+func nextString() string {
+	stdin.Scan()
+	return stdin.Text()
+}
+
+// 遅いから極力使わない。
+func nextBytes() []byte {
+	return []byte(nextString())
+}
+
+func nextInt() int {
+	i, _ := strconv.Atoi(nextString())
+	return i
+}
+
+func nextInts(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = nextInt()
+	}
+	return a
+}
+
+// toi は byteの数値をintに変換します。
+func toi(b byte) int {
+	return int(b - '0')
+}
+
+func nextLongIntAsArray() []int {
+	s := nextString()
+	l := len(s)
+	arr := make([]int, l)
+	for i := 0; i < l; i++ {
+		arr[i] = toi(s[i])
+	}
+
+	return arr
+}
+
+func nextFloat() float64 {
+	f, _ := strconv.ParseFloat(nextString(), 64)
+	return f
+}
+
+// ==================================================
+// 数値操作
+// ==================================================
+
+// max は aとbのうち大きい方を返します。
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// min は aとbのうち小さい方を返します。
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// abs は aの絶対値を返します。
+func abs(a int) int {
+	if a > 0 {
+		return a
+	}
+	return -a
+}
+
+// pow は aのb乗を返します。
+func pow(a, b int) int {
+	return int(math.Pow(float64(a), float64(b)))
+}
+
+// binarysearch は judgeがtrueを返す最小の数値を返します。
+func binarysearch(ok, ng int, judge func(int) bool) int {
+	for abs(ok-ng) > 1 {
+		mid := (ok + ng) / 2
+
+		if judge(mid) {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+
+	return ok
+}
+
+// ch は condがtrueのときok, falseのときngを返します。
+func ch(cond bool, ok, ng int) int {
+	if cond {
+		return ok
+	}
+	return ng
+}
+
+// ==================================================
+// ビット操作
+// ==================================================
+
+// nthbit はaのn番目のビットを返します。
+func nthbit(a int, n int) int { return int((a >> uint(n)) & 1) }
+
+// popcount はaのうち立っているビットを数えて返します。
+func popcount(a int) int {
+	return bits.OnesCount(uint(a))
+}
+
+// ==================================================
+// 文字列操作
+// ==================================================
+
+// toLowerCase は sをすべて小文字にした文字列を返します。
+func toLowerCase(s string) string {
+	return strings.ToLower(s)
+}
+
+// toUpperCase は sをすべて大文字にした文字列を返します。
+func toUpperCase(s string) string {
+	return strings.ToUpper(s)
+}
+
+// isLower はbが小文字かどうかを判定します
+func isLower(b byte) bool {
+	return 'a' <= b && b <= 'z'
+}
+
+// isUpper はbが大文字かどうかを判定します
+func isUpper(b byte) bool {
+	return 'A' <= b && b <= 'Z'
+}
+
+// ==================================================
+// 構造体
+// ==================================================
+
+// Point は 座標を表す構造体です。
+type Point struct {
+	x int
+	y int
+}
+
+// Pointf は座標を表す構造体です。
+type Pointf struct {
+	x float64
+	y float64
+}
