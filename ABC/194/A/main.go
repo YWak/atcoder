65d13e5f7b78dd1b68caf7c6f84333a873e21b33
YWak <ywak1984@gmail.com>
2021-03-06 21:03:40 +0900

    提出した

--- ABC/194/A/main.go
+++ ABC/194/A/main.go
@@ -19,11 +19,11 @@ const INF9 = int(1e9)
 
 func main() {
 	a, b := nextInt2()
-	if a >= 15 && b >= 8 {
+	if a+b >= 15 && b >= 8 {
 		fmt.Println(1)
-	} else if a >= 10 && b >= 3 {
+	} else if a+b >= 10 && b >= 3 {
 		fmt.Println(2)
-	} else if a >= 3 {
+	} else if a+b >= 3 {
 		fmt.Println(3)
 	} else {
 		fmt.Println(4)
