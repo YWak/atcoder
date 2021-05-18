d48a59702e5c7f508a28f33dcd6743b37a4de378
YWak <ywak1984@gmail.com>
2021-05-19 03:15:31 +0900

    コメントを追加した

--- .vscode/template.go
+++ .vscode/template.go
@@ -47,6 +47,7 @@ type Out struct {
 	writer *bufio.Writer
 }
 
+// InitIo は inとoutを初期化します。
 func InitIo() (*In, *Out) {
 	bufsize := 4 * 1024 * 1024 // 1 MB
 	in := bufio.NewScanner(os.Stdin)
@@ -58,33 +59,50 @@ func InitIo() (*In, *Out) {
 	return &In{in}, &Out{out}
 }
 
+// NextString は 次の入力を文字列として読み込んで返します。
 func (in *In) NextString() string {
 	in.reader.Scan()
 	return in.reader.Text()
 }
 
+// NextBytes は 次の入力をbyteの配列として読み込んで返します。
 // 遅いから極力使わない。
 func (in *In) NextBytes() []byte {
 	return []byte(in.NextString())
 }
 
+// NextInt は 次の入力を数値として読み込んで返します。
 func (in *In) NextInt() int {
 	i, _ := strconv.Atoi(in.NextString())
 	return i
 }
 
+// NextInt2 は 次の2つの入力を数値として読み込んで返します。
 func (in *In) NextInt2() (int, int) {
 	return in.NextInt(), in.NextInt()
 }
 
+// NextInt2d は 次の2つの入力を数値n1,n2として読み込んで、n1+d1, n2+d2を返します。
+func (in *In) NextInt2d(d1, d2 int) (int, int) {
+	return in.NextInt() + d1, in.NextInt() + d2
+}
+
+// NextInt3 は 次の3つの入力を数値として読み込んで返します。
 func (in *In) NextInt3() (int, int, int) {
 	return in.NextInt(), in.NextInt(), in.NextInt()
 }
 
-func (in *In) nextInt4() (int, int, int, int) {
+// NextInt2d は 次の3つの入力を数値n1,n2,n3として読み込んで、n1+d1, n2+d2, n3+d3を返します。
+func (in *In) NextInt3d(d1, d2, d3 int) (int, int, int) {
+	return in.NextInt() + d1, in.NextInt() + d2, in.NextInt() + d3
+}
+
+// NextInt4 は 次の4つの入力を数値として読み込んで返します。
+func (in *In) NextInt4() (int, int, int, int) {
 	return in.NextInt(), in.NextInt(), in.NextInt(), in.NextInt()
 }
 
+// NextInts は 次のn個の入力を数値として読み込んで、配列として返します。
 func (in *In) NextInts(n int) sort.IntSlice {
 	a := make([]int, n)
 	for i := 0; i < n; i++ {
@@ -93,52 +111,63 @@ func (in *In) NextInts(n int) sort.IntSlice {
 	return sort.IntSlice(a)
 }
 
-// toi は byteの数値をintに変換します。
-func toi(b byte) int {
-	return int(b - '0')
-}
-
+// NextLongIntAsArray は 次の入力を数値として読み込み、各桁を要素とした配列を返します。
 func (in *In) NextLongIntAsArray() []int {
 	s := in.NextString()
 	l := len(s)
 	arr := make([]int, l)
 	for i := 0; i < l; i++ {
-		arr[i] = toi(s[i])
+		arr[i] = int(s[i] - '0')
 	}
 
 	return arr
 }
 
+// NextFloat は 次の入力を実数値として読み込み、値を返します。
 func (in *In) NextFloat() float64 {
 	f, _ := strconv.ParseFloat(in.NextString(), 64)
 	return f
 }
 
-// NextFloatAsInt は 数を 10^base 倍した整数値を取得します。
+// NextFloatAsInt は次の入力を実数rとして読み込み、r * 10^base の値を返します。
 func (in *In) NextFloatAsInt(base int) int {
+	if base%10 == 0 {
+		panic("baseは小数点の最大桁数を指定する")
+	}
+
 	s := in.NextString()
 	index := strings.IndexByte(s, '.')
+
+	// 小数点がなければそのまま返す
 	if index == -1 {
 		n, _ := strconv.Atoi(s)
 		return n * pow(10, base)
 	}
+
+	// 末尾の0を消しておく
 	for s[len(s)-1] == '0' {
 		s = s[:len(s)-1]
 	}
+
+	// 整数部分 * 10^base + 小数部分 * 10^(足りない分)
 	s1, s2 := s[:index], s[index+1:]
 	n, _ := strconv.Atoi(s1)
 	m, _ := strconv.Atoi(s2)
+
 	return n*pow(10, base) + m*pow(10, base-len(s2))
 }
 
+// Println は引数をスペース区切りで出力し、最後に改行を出力します。
 func (out *Out) Println(a ...interface{}) {
 	fmt.Fprintln(out.writer, a...)
 }
 
+// Printf はformatにしたがってaを整形して出力します。
 func (out *Out) Printf(format string, a ...interface{}) {
 	fmt.Fprintf(out.writer, format, a...)
 }
 
+// PrintStringsln は文字列配列の各要素をスペース区切りで出力し、最後に改行を出力します。
 func (out *Out) PrintStringsln(a []string) {
 	b := make([]interface{}, len(a))
 	for i, v := range a {
@@ -147,12 +176,22 @@ func (out *Out) PrintStringsln(a []string) {
 	out.Println(b...)
 }
 
+// PrintIntsLn は整数配列の各要素をスペース区切りで出力し、最後に改行を出力します。
 func (out *Out) PrintIntsLn(a []int) {
 	b := make([]interface{}, len(a))
 	for i, v := range a {
 		b[i] = v
 	}
 	out.Println(b...)
+}
+
+// YesNo は condが真ならYes, 偽ならNoを出力します。
+func (out *Out) YesNo(cond bool) {
+	if cond {
+		out.Println("Yes")
+	} else {
+		out.Println("No")
+	}
 }
 
 // ==================================================
