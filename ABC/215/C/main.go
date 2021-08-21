446fe7ee1b92c3c63cd8e15c8ac4ea5070dbcbb7
YWak <ywak1984@gmail.com>
2021-08-21 21:12:49 +0900

    提出した

--- ABC/215/C/main.go
+++ ABC/215/C/main.go
@@ -23,14 +23,58 @@ var in *In
 var out *Out
 
 func calc() {
-	n := in.NextInt()
-	k := 0
-	for ; k < 64; k++ {
-		if (1 << k) > n {
-			out.Println(k - 1)
-			return
+	s := in.NextString()
+	k := in.NextInt()
+	n := len(s)
+	h := map[string]bool{}
+
+	o := make(Permutation, n)
+	for i := 0; i < n; i++ {
+		o[i] = i
+	}
+
+	for {
+		str := make([]byte, n)
+		for i := 0; i < n; i++ {
+			str[i] = s[o[i]]
+		}
+		h[string(str)] = true
+		if !o.next() {
+			break
 		}
 	}
+
+	ss := make([]string, 0)
+	for v := range h {
+		ss = append(ss, v)
+	}
+	sort.Slice(ss, func(i, j int) bool { return ss[i] < ss[j] })
+
+	out.Println(ss[k-1])
+}
+
+type Permutation []int
+
+func (p *Permutation) next() bool {
+	for i := len(*p) - 2; i >= 0; i-- {
+		if (*p)[i] > (*p)[i+1] {
+			continue
+		}
+		j := len(*p)
+		for {
+			j--
+			if (*p)[i] < (*p)[j] {
+				break
+			}
+		}
+		(*p)[i], (*p)[j] = (*p)[j], (*p)[i]
+		for k, l := i+1, len(*p)-1; k < l; k, l = k+1, l-1 {
+			(*p)[k], (*p)[l] = (*p)[l], (*p)[k]
+		}
+		return true
+
+	}
+	return false
 }
 
 func main() {
