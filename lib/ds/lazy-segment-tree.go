a038fba1ddae6bc05f76515eb7ac9dc521317767
YWak <ywak1984@gmail.com>
2023-11-16 10:42:29 +0900

    提出した

---
+++ lib/ds/lazy-segment-tree.go
@@ -0,0 +1,157 @@
+package ds
+
+type LazySegmentTree[V, F any] struct {
+	// Initは長さnの配列として初期化します
+	Init func(n int) *LazySegmentTree[V, F]
+
+	// InitByArrayはarrとして初期化します。
+	InitByArray func(arr []V) *LazySegmentTree[V, F]
+
+	// Updateは[l, r)をfで更新します。
+	Update func(l, r int, f F)
+
+	// Queryは[l, r)の値を返します。
+	Query func(l, r int) V
+}
+
+// NewLazySegmentTreeは遅延評価セグメントツリーの実装を返します。
+// Vは扱うモノイドの型、Fはモノイドに適用する写像をあらわします。
+func NewLazySegmentTree[V, F any](
+	operate func(a, b V) V,
+	mapping func(f F, x V) V,
+	composition func(f, g F) F,
+	e func() V,
+	id func() F,
+) *LazySegmentTree[V, F] {
+	st := &LazySegmentTree[V, F]{}
+	var log int
+	var size int
+	var data []V
+	var lazy []F
+
+	update := func(k int) {
+		data[k] = operate(data[k*2+0], data[k*2+1])
+	}
+
+	// 初期化
+	// tested:
+	//   https://atcoder.jp/contests/typical90/tasks/typical90_ac
+	st.Init = func(n int) *LazySegmentTree[V, F] {
+		arr := make([]V, n)
+		for i := 0; i < n; i++ {
+			arr[i] = e()
+		}
+		return st.InitByArray(arr)
+	}
+
+	// 配列による初期化
+	// tested:
+	//   https://atcoder.jp/contests/typical90/tasks/typical90_ac
+	st.InitByArray = func(arr []V) *LazySegmentTree[V, F] {
+		n := len(arr)
+		size = 1
+		log = 0
+		for size < n {
+			size *= 2
+			log++
+		}
+		data = make([]V, size*2)
+		for i := 0; i < n; i++ {
+			data[size+i] = arr[i]
+		}
+		for i := size - 1; i >= 1; i-- {
+			update(i)
+		}
+		lazy = make([]F, size)
+		for i := range lazy {
+			lazy[i] = id()
+		}
+
+		return st
+	}
+
+	applyAll := func(k int, f F) {
+		data[k] = mapping(f, data[k])
+		if k < size {
+			lazy[k] = composition(f, lazy[k])
+		}
+	}
+	push := func(k int) {
+		applyAll(2*k+0, lazy[k])
+		applyAll(2*k+1, lazy[k])
+		lazy[k] = id()
+	}
+	// [l, r)の値を取得する。
+	// tested:
+	//   https://atcoder.jp/contests/typical90/tasks/typical90_ac
+	st.Query = func(l, r int) V {
+		if l == r {
+			return e()
+		}
+		l, r = l+size, r+size
+		for i := log; i >= 1; i-- {
+			if (l>>i)<<i != l {
+				push(l >> i)
+			}
+			if (r>>i)<<i != r {
+				push(r >> i)
+			}
+		}
+		sml, smr := e(), e()
+		for l < r {
+			if l%2 == 1 {
+				sml = operate(sml, data[l])
+				l++
+			}
+			if r%2 == 1 {
+				r--
+				smr = operate(data[r], smr)
+			}
+			l >>= 1
+			r >>= 1
+		}
+
+		return operate(sml, smr)
+	}
+
+	// [l, r)の値をfで更新する
+	// tested:
+	//   https://atcoder.jp/contests/typical90/tasks/typical90_ac
+	st.Update = func(l, r int, f F) {
+		if l == r {
+			return
+		}
+		l, r = l+size, r+size
+		for i := log; i >= 1; i-- {
+			if (l>>i)<<i != l {
+				push(l >> i)
+			}
+			if (r>>i)<<i != r {
+				push((r - 1) >> i)
+			}
+		}
+		ll, rr := l, r
+		for ll < rr {
+			if ll%2 == 1 {
+				applyAll(ll, f)
+				ll++
+			}
+			if rr%2 == 1 {
+				rr--
+				applyAll(rr, f)
+			}
+			ll >>= 1
+			rr >>= 1
+		}
+		for i := 1; i <= log; i++ {
+			if (l>>i)<<i != l {
+				update(l >> i)
+			}
+			if (r>>i)<<i != r {
+				update((r - 1) >> i)
+			}
+		}
+	}
+
+	return st
+}
