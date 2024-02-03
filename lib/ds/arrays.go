996c87b28f1def52644a84d3227160ce5be9064d
YWak <ywak1984@gmail.com>
2024-02-04 01:44:11 +0900

    提出した

--- lib/ds/arrays.go
+++ lib/ds/arrays.go
@@ -1,18 +1,28 @@
 package ds
 
-func NewInt2d(a, b, value int) [][]int {
-	arr := make([][]int, a)
-	for i := range arr {
-		arr[i] = make([]int, b)
+type Int2d [][]int
 
-		if value != 0 {
-			for j := range arr[i] {
-				arr[i][j] = value
-			}
+func NewInt2d(a, b, value int) [][]int {
+	base := make([]int, a*b)
+	if value != 0 {
+		for i := range base {
+			base[i] = value
 		}
 	}
+	arr := make([][]int, a)
+	for i := range arr {
+		arr[i] = base[(i * b):((i + 1) * b)]
+	}
 
-	return arr
+	return Int2d(arr)
+}
+
+func (arr *Int2d) Init(v int) {
+	_a := (*arr)
+	a := _a[0][0 : len(_a)*len(_a[0])]
+	for i := 0; i < len(a); i++ {
+		a[i] = v
+	}
 }
 
 func NewInt3d(a, b, c, value int) [][][]int {
@@ -20,6 +30,30 @@ func NewInt3d(a, b, c, value int) [][][]int {
 	for i := range arr {
 		arr[i] = NewInt2d(b, c, value)
 	}
+	return arr
+}
+
+type Int4d [][][][]int
+
+func NewInt4d(a, b, c, d, value int) Int4d {
+	base := make([]int, a*b*c*d)
+	if value != 0 {
+		for i := range base {
+			base[i] = value
+		}
+	}
+	arr := make(Int4d, a)
+	for i := range arr {
+		arr[i] = make([][][]int, b)
+		for j := range arr[i] {
+			arr[i][j] = make([][]int, c)
+			for k := range arr[i][j] {
+				p := i*b*c*d + j*c*d + k*d
+				arr[i][j][k] = base[p:(p + d)]
+			}
+		}
+	}
+
 	return arr
 }
 
