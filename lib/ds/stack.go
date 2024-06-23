b7f73d312411d8839fe126535daf45fcd0394043
ywak <ywak1984@gmail.com>
2024-06-23 17:38:09 +0900

    提出した

--- lib/ds/stack.go
+++ lib/ds/stack.go
@@ -3,49 +3,56 @@ package ds
 // Stackは先入れ後出しのデータ構造を表現します。
 type Stack[T any] struct {
 	arr []T
-}
 
-// PushはこのStackの末尾に要素を追加します。
-func (stack *Stack[T]) Push(value T) {
-	stack.arr = append(stack.arr, value)
-}
+	// PushはこのStackの末尾に要素を追加します。
+	Push func(value T)
 
-// PopはこのStackの末尾から要素を取り出し、その要素を返します。
-func (stack *Stack[T]) Pop() T {
-	if len(stack.arr) == 0 {
-		panic("Pop() is called for empty stack")
-	}
+	// PopはこのStackの末尾から要素を取り出し、その要素を返します。
+	Pop func() T
 
-	value := stack.Peek()
-	stack.arr = stack.arr[:len(stack.arr)-1]
-	return value
-}
+	// PeekはこのStackの末尾の要素を返します。
+	// 末尾の要素は削除されません。
+	Peek func() T
 
-// PeekはこのStackの末尾の要素を返します。
-// 末尾の要素は削除されません。
-func (stack *Stack[T]) Peek() T {
-	if len(stack.arr) == 0 {
-		panic("Peek() is called for empty stack")
-	}
+	// CountはこのStackに保存されている要素の数を返します。
+	Count func() int
 
-	return stack.arr[len(stack.arr)-1]
-}
+	// HasElementsはこのStackに要素が設定されているかを判断します。
+	HasElements func() bool
 
-// CountはこのStackに保存されている要素の数を返します。
-func (stack *Stack[T]) Count() int {
-	return len(stack.arr)
-}
-
-// HasElementsはこのStackに要素が設定されているかを判断します。
-func (stack *Stack[T]) HasElements() bool {
-	return stack.Count() > 0
-}
-
-// IsEmptyはこのStackが空かどうかを判断します。
-func (stack *Stack[T]) IsEmpty() bool {
-	return !stack.HasElements()
+	// IsEmptyはこのStackが空かどうかを判断します。
+	IsEmpty func() bool
 }
 
 func NewStack[T any]() *Stack[T] {
-	return &Stack[T]{}
+	stack := Stack[T]{}
+	stack.Push = func(value T) {
+		stack.arr = append(stack.arr, value)
+	}
+	stack.Pop = func() T {
+		if len(stack.arr) == 0 {
+			panic("Pop() is called for empty stack")
+		}
+
+		value := stack.Peek()
+		stack.arr = stack.arr[:len(stack.arr)-1]
+		return value
+	}
+	stack.Peek = func() T {
+		if len(stack.arr) == 0 {
+			panic("Peek() is called for empty stack")
+		}
+
+		return stack.arr[len(stack.arr)-1]
+	}
+	stack.Count = func() int {
+		return len(stack.arr)
+	}
+	stack.HasElements = func() bool {
+		return stack.Count() > 0
+	}
+	stack.IsEmpty = func() bool {
+		return stack.Count() == 0
+	}
+	return &stack
 }
