9cbd23aa962891263441b66e16ad3a025a412d2f
YWak <ywak1984@gmail.com>
2025-05-12 03:50:19 +0900

    完備辞書を追加した

--- lib/ds/succinct-indexable-dictionary.go
+++ lib/ds/succinct-indexable-dictionary.go
@@ -1,46 +1,108 @@
 package ds
 
-import "math/bits"
+import (
+	"math/bits"
+
+	"github.com/ywak/atcoder/lib/math"
+)
+
+const LEVEL_L = 65536
+const LEVEL_S = 256
 
 // 完備辞書
 type SuccinctIndexableDictionary struct {
-	length int
-	bits   []uint32
-	sum    []uint32
+	size  int
+	bits  []uint64
+	large []uint64
+	small []uint16
+	ones  int
 }
 
-func NewSuccinctIndexableDictionary(length int) *SuccinctIndexableDictionary {
-	blocks := (length + 31) >> 5
+func NewSuccinctIndexableDictionary(size int) *SuccinctIndexableDictionary {
+	s := (size+64-1)/64 + 1
+
 	return &SuccinctIndexableDictionary{
-		length: length,
-		bits:   make([]uint32, blocks),
-		sum:    make([]uint32, blocks),
+		size:  size,
+		bits:  make([]uint64, s),
+		large: make([]uint64, s/LEVEL_L+1),
+		small: make([]uint16, s/LEVEL_S+1),
 	}
 }
 
-func (s *SuccinctIndexableDictionary) Set(i int) {
-	s.bits[i>>5] |= 1 << uint(i&31)
-}
+// Setはi番目のビットをbitにします。
+func (s *SuccinctIndexableDictionary) Set(i int, bit int) {
+	pos, offset := s.index(i)
 
-func (s *SuccinctIndexableDictionary) Build() {
-	s.sum[0] = 0
-	for i := 1; i < len(s.sum); i++ {
-		s.sum[i] = s.sum[i-1] + uint32(bits.OnesCount32(s.bits[i-1]))
+	if bit == 1 {
+		s.bits[pos] |= 1 << offset
+	} else {
+		s.bits[pos] &= (^(1 << offset))
 	}
 }
 
 func (s *SuccinctIndexableDictionary) Get(i int) int {
-	return int(s.bits[i>>5]>>uint(i&31)) & 1
+	pos, offset := s.index(i)
+	return int(s.bits[pos]>>offset) & 1
 }
 
-func (s *SuccinctIndexableDictionary) Rank(i int) int {
-	return (int(s.sum[i>>5]) + bits.OnesCount32(s.bits[i>>5]&((1<<uint(i&31))-1)))
+func (s *SuccinctIndexableDictionary) index(i int) (int, int) {
+	return i / 64, i % 64
 }
 
-func (s *SuccinctIndexableDictionary) Rank0(i int) int {
-	return i - s.Rank(i)
+func (s *SuccinctIndexableDictionary) Build() {
+	num := 0
+	for j, b := range s.bits {
+		i := j * 64
+		if i%LEVEL_L == 0 {
+			s.large[i/LEVEL_L] = uint64(num)
+		}
+		if i%LEVEL_S == 0 {
+			s.small[i/LEVEL_S] = uint16(num - int(s.large[i/LEVEL_L]))
+		}
+		num += bits.OnesCount64(b)
+	}
+	s.ones = num
 }
 
-func (s *SuccinctIndexableDictionary) Rank1(i int) int {
-	return s.Rank(i)
+// Rankは[0, i)に含まれるbitの数を返す。
+func (s *SuccinctIndexableDictionary) Rank(bit, i int) int {
+	if bit == 1 {
+		return s.rank1(i)
+	} else {
+		return i - s.rank1(i)
+	}
+}
+
+// [0, i)の1の数を返す。
+func (s *SuccinctIndexableDictionary) rank1(i int) int {
+	// LEVEL_L単位 + LEVEL_S単位 + 64bit単位 + 残り
+
+	rank := s.large[i/LEVEL_L] + uint64(s.small[i/LEVEL_S])
+	begin := (i / LEVEL_S) * LEVEL_S / 64
+	end := i / 64
+
+	for t := begin; t < end; t++ {
+		rank += uint64(bits.OnesCount64(s.bits[t]))
+	}
+	rem := (i % LEVEL_S) % 64
+	rank += uint64(bits.OnesCount64(s.bits[end] & ((1 << rem) - 1)))
+
+	return int(rank)
+}
+
+// rank番目のbitの位置 + 1を返す。rankは1-origin。
+func (s *SuccinctIndexableDictionary) Select(bit, rank int) int {
+	// 暫定対応でrankを使って二分探索する
+	// s.Rank(bit, ok+1) == rankとなるような最小のokを求める
+	ng, ok := 0, s.size+1
+	for math.Abs(ok-ng) > 1 {
+		mid := (ok + ng) / 2
+		if s.Rank(bit, mid) >= rank {
+			ok = mid
+		} else {
+			ng = mid
+		}
+	}
+
+	return ok
 }
---
+++ lib/ds/succinct-indexable-dictionary_test.go
@@ -0,0 +1,61 @@
+package ds_test
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+
+	"github.com/ywak/atcoder/lib/ds"
+)
+
+func TestSuccinctIndexableDictionary(t *testing.T) {
+	size := 100
+	for q := 0; q < 100; q++ {
+		s := ds.NewSuccinctIndexableDictionary(size)
+		arr := make([]int, size)
+		for i := 0; i < size; i++ {
+			arr[i] = rand.Intn(2)
+			s.Set(i, arr[i])
+		}
+		t.Log(arr)
+
+		s.Build()
+
+		t.Run(fmt.Sprintf("Get(%d)", q), func(t *testing.T) {
+			for i := 0; i < size; i++ {
+				actual := s.Get(i)
+				if actual != arr[i] {
+					t.Errorf("Get(%d) = %d, want %d", i, actual, arr[i])
+				}
+			}
+		})
+		t.Run(fmt.Sprintf("Rank(%d)", q), func(t *testing.T) {
+			cnt := [2]int{}
+
+			for i := 0; i < size; i++ {
+				r0 := s.Rank(0, i)
+				if r0 != cnt[0] {
+					t.Errorf("Rank(0, %d) = %d, want %d", i, r0, cnt[0])
+				}
+
+				r1 := s.Rank(1, i)
+				if r1 != cnt[1] {
+					t.Errorf("Rank(1, %d) = %d, want %d", i, r1, cnt[1])
+				}
+
+				cnt[arr[i]]++
+			}
+		})
+		t.Run(fmt.Sprintf("Select(%d)", q), func(t *testing.T) {
+			cnt := [2]int{}
+			for i := 0; i < size; i++ {
+				cnt[arr[i]]++ // iはi+1番目のarr[i]の位置
+
+				s := s.Select(arr[i], cnt[arr[i]])
+				if s != i+1 {
+					t.Errorf("Select(%d, %d) = %d, want %d", arr[i], cnt[arr[i]], s, i+1)
+				}
+			}
+		})
+	}
+}
